dccadfad570e4a4f44251b717a1d3340a0b20240
Willy Bresee <wsbresee@gmail.com>
2020-07-24 17:49:32 -0400

    got flask working, maybe db working on arch install

--- app.py
+++ app.py
@@ -2,29 +2,24 @@ from flask import Flask, render_template, request, make_response, session, escap
 from flask_wtf.csrf import CSRFProtect
 from subprocess import check_output
 import os
+from passlib.hash import sha256_crypt
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 INPUTTEXT_ADDR = 'input.txt'
 DICTIONARY_ADDR = 'wordlist.txt'
 SECRET_KEY = os.urandom(32)
-DATABASE_FILE = "database.db"
-
-class User:
-    def __init__(self, username, password, phone):
-        self.username = username
-        self.password = password
-        self.phone = phone
-        self.is_logged_in = False
+DATABASE_FILE = "sqlite:///" + os.path.join(os.getcwd(), "database.db")
 
 users = []
 
-def get_user(the_user):
-    for a_user in users:
-        if a_user.username == the_user.username:
-            return a_user
+def getUser(username):
+    return UserRecord.query.filter_by(username=username).first()
 
 app = Flask(__name__)
 app.secret_key = os.urandom(32)
 app.config.update(SQLALCHEMY_DATABASE_URI = DATABASE_FILE)
+app.config.update(SQLALCHEMY_TRACK_MODIFICATIONS = False)
 csrf = CSRFProtect(app)
 db = SQLAlchemy(app)
 
@@ -38,10 +33,17 @@ class UserRecord(db.Model):
 class LoginRecord(db.Model):
     __tablename__ = "login_records"
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    user_id = db.Column(db.Integer)
+    login_time = db.Column(db.DateTime)
 
 class SpellCheckRecord(db.Model):
     __tablename__ = "spell_check_records"
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    user_id = db.Column(db.Integer)
+    inputtext = db.Column(db.String(64))
+    misspelled = db.Column(db.String(64))
+
+db.create_all()
 
 @app.route('/')
 def index():
@@ -54,12 +56,10 @@ def spell_check():
     if (request.method == 'POST'):
         if 'username' in session:
             username = session['username']
-            password = session['password']
-            phone = session['phone']
-            user = get_user(User(username, password, phone))
+            user = getUser(username)
         else:
             user = None
-        if user and user.password == password and user.phone == phone and user.is_logged_in:
+        if user:
             inputtext = request.form['inputtext']
             inputtext_file = open(INPUTTEXT_ADDR, 'w')
             inputtext_file.write(inputtext)
@@ -67,6 +67,9 @@ def spell_check():
             textout=inputtext
             misspelled = check_output(['./a.out', INPUTTEXT_ADDR, DICTIONARY_ADDR]).decode('utf-8')
             misspelled = misspelled.replace('\n', ',').strip(',')
+            spell_check_record = SpellCheckRecord(user_id=user.id, inputtext=inputtext, misspelled=misspelled)
+            db.session.add(spell_check_record)
+            db.commit()
         else:
             textout = "Invalid user. Please log in."
     response = make_response(render_template('spell_check.html', textout=textout, misspelled=misspelled))
@@ -87,20 +90,22 @@ def login():
         elif not phone:
             error = 'invalid phone'
         else:
-            user = User(username, password, phone)
-            maybe_the_user = get_user(user)
-            if not maybe_the_user:
+            user = getUser(username)
+            if not user:
                 error = 'Incorrect'
-            elif (maybe_the_user.password != user.password):
+            elif (not sha256_crypt.verify(password, user.password)):
                 error = 'Incorrect'
-            elif (maybe_the_user.phone != user.phone):
+            elif (not phone == user.phone):
                 error = 'Two-factor failure'
             else:
                 error = 'success'
                 session['username'] = username
                 session['password'] = password
                 session['phone'] = phone
-                maybe_the_user.is_logged_in = True
+                session['user_id'] = user.id
+                login = LoginRecord(user_id=user.id, login_time=datetime.utcnow())
+                db.session.add(login)
+                db.commit()
     response = make_response(render_template('login.html', error=error))
     return response
 
@@ -118,9 +123,10 @@ def register():
         elif not phone:
             error = 'invalid phone'
         else:
-            user = User(username, password, phone)
-            if not get_user(user):
-                users.append(user)
+            if not getUser(username):
+                user = UserRecord(username=username, password=password, phone=phone)
+                db.session.add(user)
+                db.session.commit()
                 error = 'success'
             else:
                 error = 'failure'
